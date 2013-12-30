2b43986d1ef55300dc268b080fce06753edacab1
xmx0632 <xmx0632@gmail.com>
2013-12-30 21:23:26 +0800

    增加log压缩命令调用

--- grails-app/domain/commandagent/Command.groovy
+++ grails-app/domain/commandagent/Command.groovy
@@ -10,6 +10,7 @@ class Command {
     		"/mnt/e6/workspace/upgrade.sh", 
     		"/mnt/e6/workspace/nginx_start.sh", 
     		"/mnt/e6/workspace/nginx_stop.sh", 
+    		"/mnt/e6/workspace/zip_log.sh",
     		"/mnt/e6/workspace/tomcat_start.sh",
     		"/mnt/e6/workspace/tomcat_stop.sh"
     		])
